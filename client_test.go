a6884edc794e5b82ecef6e5b02fba204fbee7593
gexiao <gxgexiao@gmail.com>
2022-10-28 23:17:43 +0800

    Update test

--- client_test.go
+++ client_test.go
@@ -44,26 +44,32 @@ func TestClient_SubscribePrice(t *testing.T) {
 	assert.NoError(t, err)
 	count := 0
 	notify := make(chan struct{})
-	for price := range ch {
-		found := false
-		t.Logf("%#v", *price)
-		for _, pair := range pairs {
-			if price.Pair == pair {
-				found = true
-				count++
+	go func() {
+		for price := range ch {
+			found := false
+			t.Logf("%#v", *price)
+			for _, pair := range pairs {
+				if price.Pair == pair {
+					found = true
+					count++
+					break
+				}
+			}
+			assert.True(t, found)
+			if count >= 10 {
+				close(notify)
 				break
 			}
 		}
-		assert.True(t, found)
-		if count >= 4 {
-			close(notify)
-			break
-		}
-	}
+	}()
+
 	select {
 	case <-notify:
 	case <-time.After(20 * time.Second):
 		assert.Errorf(t, assert.AnError, "timeout")
+	case err := <-errC:
+		t.Log(err)
+		panic(err)
 	}
 	cancel()
 	time.Sleep(2 * time.Second)
